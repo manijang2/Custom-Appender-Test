a703ceade904a7fe11d50c2c5718ca2d3185ce6a
seonman.kim <seonman.kim@jnfirst.co.kr>
2024-05-30 15:54:04 +0900

    Initial commit

---
+++ src/main/java/com/demo/Application.java
@@ -0,0 +1,14 @@
+package com.demo;
+
+import com.demo.appender.CustomAppender;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
